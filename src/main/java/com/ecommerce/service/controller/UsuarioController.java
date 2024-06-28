e2ebc08eacb25103194d32d464f9e945edbbf922
Facundoo15 <fangulos1@upao.edu.pe>
2024-06-28 12:32:04 -0500

    feat: Implementaćión de la clase email

--- src/main/java/com/ecommerce/service/controller/UsuarioController.java
+++ src/main/java/com/ecommerce/service/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.service.controller;
 
 import com.ecommerce.service.entity.Usuario;
+import com.ecommerce.service.service.EmailSenderService;
 import com.ecommerce.service.service.UsuarioService;
 import com.ecommerce.service.utility.GenericResponse;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+    private EmailSenderService emailSenderService;
 
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
@@ -25,6 +28,16 @@ public class UsuarioController {
 
     @PostMapping
     public GenericResponse<?> save(@RequestBody Usuario u){
+        System.out.println(u);
+        emailSenderService.sendEmail(u.getEmail(),"Bienvenido a El Mana", "¡Hola!\n" +
+                "\n" +
+                "¡Nos alegra saber que planeas visitarnos en Panadería El Maná! En nuestra panadería, nos dedicamos a ofrecerte los panes y postres más frescos y deliciosos, elaborados con ingredientes de la más alta calidad. Desde croissants y baguettes crujientes hasta pasteles y galletas irresistibles, tenemos algo para satisfacer todos tus antojos.\n" +
+                "\n" +
+                "Nuestro horario de atención es de lunes a sábado, de 7:00 a.m. a 7:00 p.m., y los domingos de 8:00 a.m. a 2:00 p.m. Nos encontramos en [dirección de la panadería].\n" +
+                "\n" +
+                "Estamos ansiosos por darte la bienvenida y hacer de tu visita una experiencia deliciosa.\n" +
+                "\n" +
+                "¡Te esperamos pronto!");
         return this.usuarioService.guardarUsuario(u);
     }
     @PutMapping("/{id}")
---
+++ src/main/java/com/ecommerce/service/service/EmailSenderService.java
@@ -0,0 +1,30 @@
+package com.ecommerce.service.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailSenderService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(String toEmail, String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("panderiaelmanatrux@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        mailSender.send(message);
+
+        System.out.println("Mail Sent success: ");
+
+    }
+
+}
